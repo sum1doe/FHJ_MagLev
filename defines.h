8be96a1b2e8d6f04377986321b364e63bdb0d3db
sum1doe <debeerdanre@gmail.com>
2026-04-14 15:47:02 +0200

    Moving contents from PID_VALS to defines.h

--- defines.h
+++ defines.h
@@ -9,12 +9,42 @@
 #define DEFINES_H_
 
 
+// Units in whatever the BoostXL board reads.
+#define MinVoltage 850
+#define RecoverVoltage 900
+
+////////////////
+//
+//   PID Values 
+//
+////////////////
+
+#define POS_P 1
+#define POS_I 0
+#define POS_D 0
+
+#define VEL_P 1
+#define VEL_I 0
+#define VEL_D 0
+
+#define ACC_P 1
+#define ACC_I 0
+#define ACC_D 0
+
+#define CUR_P 1
+#define CUR_I 0
+#define CUR_D 0
+
+//========================//
+
+
 #define BS              0x08
 
 #define STEP_FULL       1
 #define STEP_HALF       2
 #define STEP_MICRO8     3
 #define STEP_MICRO16    4
+
 
 
 #endif /* DEFINES_H_ */
