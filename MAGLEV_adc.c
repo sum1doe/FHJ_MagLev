dda6353255702b643983fa4a15fc1729421cf19a
sum1doe <debeerdanre@gmail.com>
2026-04-21 11:20:56 +0200

    Found a missed debug assignment

--- MAGLEV_adc.c
+++ MAGLEV_adc.c
@@ -272,7 +272,7 @@ interrupt void  ISRadc(void)
 
     currentcurrent = 2100-((int16)tempADC[5-dir]);
     if (currentcurrent < 0) currentcurrent = 0;
-    debug = currentcurrent;
+    // debug = currentcurrent;
     currentcurrent = (int16) shunt2current(currentcurrent);
 
     currentcurrent = currentcurrent * m + prevCurrent * (1-m);
