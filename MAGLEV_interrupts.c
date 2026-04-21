dbdb6c72c8aba556a86fb3518b8ca5dcb1d15df2
sum1doe <debeerdanre@gmail.com>
2026-04-21 20:13:10 +0200

    ODL: adding debug to DL decr

--- MAGLEV_interrupts.c
+++ MAGLEV_interrupts.c
@@ -65,6 +65,7 @@ interrupt void timerISR(void)
     // entered every 1ms
 
     if ((tempADC[7] < MinVoltage || currentcurrent > MaxCurrent) && DutyLimit > 0) {
+        debug = currentcurrent;
         DutyLimit--;
     }
     else if (tempADC[7] > RecoverVoltage && currentcurrent < MaxCurrent && DutyLimit < EPWM_TIMER_TBPRD) {
