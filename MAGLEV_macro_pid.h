21b4c1f9cc243ce97632ac107a53442bd80f843a
sum1doe <debeerdanre@gmail.com>
2026-08-05 15:00:24 +0200

    Cleaning up PID Macros

--- MAGLEV_macro_pid.h
+++ MAGLEV_macro_pid.h
@@ -21,14 +21,24 @@ typedef struct PIDStruct {
 void initPID(PID* pid, double k, double kp, double ki, double kd);
 void initAllPIDs();
 
-#define updatePID(pid, Data, SP) pid.prevData=pid.data;pid.data=Data;pid.sp=SP;
+#define updatePID(pid, Data, SP)\
+    pid.prevData=pid.data;\
+    pid.data=Data;\
+    pid.sp=SP;
 
 // I know it's bad practice, sorry...
-#define getCV(pid) pid.k * (doP(pid) + doI(pid) - doD(pid))
+// The \ here is to escape the newline, allowing multiline defines
+#define getCV(pid)\
+    pid.k * (doP(pid) + doI(pid) - doD(pid))
 
-#define doP(pid) pid.kp * (pid.sp - pid.data)
-#define doI(pid) (pid.prevI = pid.kM * (pid.sp - pid.data) + (1-pid.kM) * pid.prevI)
-#define doD(pid) pid.kd * (pid.data-pid.prevData)
+#define doP(pid) \
+    pid.kp * (pid.sp - pid.data)
+
+#define doI(pid) \
+    pid.prevI = pid.kM * (pid.sp - pid.data) + (1-pid.kM) * pid.prevI
+
+#define doD(pid) \
+    pid.kd * (pid.data-pid.prevData)
 
 // You only need to implement StepPIDs yourself, wherever.
 
